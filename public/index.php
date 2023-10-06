bddf20bf2ab3834eb5646786b2505d5317d11819
Asher Novelli | Plano Angelus <asher.novelli@planoangelus.com.br>
2023-10-06 18:01:46 -0300

    Slim framework configuration

--- public/index.php
+++ public/index.php
@@ -2,15 +2,33 @@
 
 require __DIR__ . "/../vendor/autoload.php";
 
+
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 use src\aplication\useCases\userLogin\InputBoundary;
 use src\aplication\useCases\userLogin\UserLogin;
 use src\infra\adapters\SessionSaveAdapter;
 use src\infra\repositories\MySQL\MySQLRepo;
+use Slim\Factory\AppFactory;
 
 
 session_start(['cookie_lifetime' => 1200, 'cookie_secure' => true, 'cookie_httponly' => true]);
 
 
+$app = AppFactory::create();
+
+$app->get('/', function (ServerRequestInterface $request, ResponseInterface $response, array $args) {
+    $response->getBody()->write("Hello, name");
+    $json = $request->getBody();
+    $tet= json_decode($json);
+    var_dump($tet);
+
+    return $response;
+});
+
+$app->run();
+/* 
 try {
     $pdo = new PDO("mysql:host=localhost;dbname=db_like", "root", "");
     $userRepo = new MySQLRepo($pdo);
@@ -21,3 +39,4 @@ try {
 } catch (Exception $e) {
     echo "<h1>DEU ERROR</h1>" . $e->getMessage();
 }
+ */
